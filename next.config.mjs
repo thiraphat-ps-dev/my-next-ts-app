3d16e04a38749dce5bc3eb13aa37e3d90324784f
thiraphat.phutson <thiraphat.phutson@kingpower.com>
2024-06-14 12:58:12 +0700

    setup rule eslint and nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+
+const nextConfig = {
+  // การตั้งค่าพื้นฐานต่างๆ ของ Next.js
+  reactStrictMode: true,
+  swcMinify: true,
+  // การปรับแต่ง Webpack
+  webpack: (config, { isServer }) => {
+    // เพิ่ม .mjs เป็นส่วนหนึ่งของการ resolve
+    config.resolve.extensions.push('.mjs');
+
+    // ตัวอย่างการใช้ alias
+    config.resolve.alias['@components'] = path.join(
+      process.cwd(),
+      'components',
+    );
+
+    // ตั้งค่า loader สำหรับไฟล์ .mjs
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
